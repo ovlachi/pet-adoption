a983d79df9a2b0ea97b34bb9e10da8cfb5845d84
georgeovlachi <g.ovlachi@gmail.com>
2024-03-31 14:32:57 +1100

    Work in progress for pets-area, fetched pets data from external JSON

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Dynamic data fetch from weather.api
+//To fetch current Temperature needs an API key from above URL
 async function start() {
   const weatherPromise = await fetch("https://api.weatherapi.com/v1/current.json?key=ed82da91afb54a018cf131037243003&q=Melbourne&aqi=no");
   const weatherData = await weatherPromise.json();
@@ -6,3 +8,16 @@ async function start() {
   document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 start();
+
+//Dynamic pets fetch from external JSON file
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+  const petsData = await petsPromise.json();
+
+  petsData.forEach((pet) => {
+    console.log(pet.species);
+  });
+}
+
+petsArea();
