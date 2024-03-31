5cf38810c560a5231be3e43d5f5feea35bc29b5f
georgeovlachi <g.ovlachi@gmail.com>
2024-03-31 14:54:05 +1100

    Work in progress, cloned pet card template and injected pet name into HTML

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 //Dynamic data fetch from weather.api
 //To fetch current Temperature needs an API key from above URL
 async function start() {
@@ -10,14 +13,23 @@ async function start() {
 start();
 
 //Dynamic pets fetch from external JSON file
-
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
   const petsData = await petsPromise.json();
 
   petsData.forEach((pet) => {
-    console.log(pet.species);
+    //clone the cart template
+    const clone = template.content.cloneNode(true);
+
+    //fetch all pet names and inject to the cloned template
+    clone.querySelector("h3").textContent = pet.name;
+
+    //add all cloned items to the wrapper
+    wrapper.appendChild(clone);
   });
+
+  //append the wrapper to the html DOM
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
