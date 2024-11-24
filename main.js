e34a7986d1c7a2ab18a077cb6fbc5add1f988465
georgeovlachi <g.ovlachi@gmail.com>
2024-11-24 14:58:45 +1100

    Changed the current temperature fetch funcrion name from start to weather

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const wrapper = document.createDocumentFragment();
 
 //Dynamic data fetch from weather.api
 //To fetch current Temperature needs an API key from above URL
-async function start() {
+async function weather() {
   const weatherPromise = await fetch("https://api.weatherapi.com/v1/current.json?key=ed82da91afb54a018cf131037243003&q=Melbourne&aqi=no");
   const weatherData = await weatherPromise.json();
-
   const ourTemperature = weatherData.current.temp_c;
   document.querySelector("#temperature-output").textContent = ourTemperature;
 }
-start();
+weather();
 
 //Dynamic pets fetch from external JSON file
 async function petsArea() {
